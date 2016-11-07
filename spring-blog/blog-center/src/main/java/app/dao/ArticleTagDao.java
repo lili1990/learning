e387ca0235ad2ebd568199e73acfb73afb2a4770
lili1990 <942212309@qq.com>
2016-11-08 00:03:25 +0800

    标签操作

--- spring-blog/blog-center/src/main/java/app/dao/ArticleTagDao.java
+++ spring-blog/blog-center/src/main/java/app/dao/ArticleTagDao.java
@@ -1,5 +1,10 @@
 package app.dao;
 
+import app.models.Article;
+import app.models.Tag;
+
+import java.util.List;
+
 /**
  * Created by sdlili on 16-10-29.
  */
@@ -9,4 +14,8 @@ public interface ArticleTagDao extends BaseDao{
     public void deleteByTag(Long tag_id);
 
     public void deleteByArticle(Long article_id);
+
+    public List<Tag> fetchByArticleId(Long articleId);
+
+    public List<Article> fetchByTagId(Long articleId);
 }
