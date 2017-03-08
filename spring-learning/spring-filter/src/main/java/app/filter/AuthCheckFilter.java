2541c5712e21211f7d5fc36b4c5fb06fc035c84f
lili1990 <942212309@qq.com>
2017-03-09 07:38:07 +0800

    过滤器完善

--- spring-learning/spring-filter/src/main/java/app/filter/AuthCheckFilter.java
+++ spring-learning/spring-filter/src/main/java/app/filter/AuthCheckFilter.java
@@ -1,6 +1,7 @@
 package app.filter;
 
 import app.service.UserService;
+import app.service.UserServiceImpl;
 import com.google.gson.Gson;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.StringUtils;
@@ -20,14 +21,14 @@ import java.io.PrintWriter;
 public class AuthCheckFilter implements Filter {
 
 
-    @Resource
+//    @Resource //不生效
     private UserService userService;
 
     public void init(FilterConfig filterConfig) throws ServletException {
        //获取容器上下文，可以进行对Bean的操作
         ServletContext context = filterConfig.getServletContext();
         ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(context);
-
+        userService = (UserServiceImpl)ctx.getBean(UserServiceImpl.class);
 
 
     }
@@ -38,6 +39,14 @@ public class AuthCheckFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse)servletResponse;
         String user_token = request.getHeader("user_token");
         String methodString=request.getMethod();
+        //这个判断只是简单的说明，通过init方法我们可以获取bean对象来进行验证操作
+        if(!userService.findOne(user_token)){
+            response.setContentType("application/json; charset=utf-8");
+            String jsonString="{\"status\":\"false\",\"info\":\"用户未登陆或超时\"}";
+            OutputStream os = response.getOutputStream();
+            os.write(jsonString.getBytes());
+            os.close();
+        }
         //这里是个简单的demo ，具体逻辑需要时再处理
         if(StringUtils.isEmpty(user_token)){
             response.setContentType("application/json; charset=utf-8");
